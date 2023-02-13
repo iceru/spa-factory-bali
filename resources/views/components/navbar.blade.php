9a744060ad8c2b4aa0dc07d49f45f35ba04c3f4d
Muhamad Hafiz <m.hafiz1825@gmail.com>
2023-02-13 08:35:46 +0700

    Setup frontend

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav>
+    <div class="container mx-auto flex justify-between items-center">
+        <div class="nav__logo">
+            <img src="/images/logo-white.png" alt="Spa Factory Bali">
+        </div>
+        <ul class="nav__menu-list flex items-center">
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/">About Us</a>
+            </li>
+            <li>
+                <a href="/">Sustainability</a>
+            </li>
+            <li>
+                <a href="/">Clientele</a>
+            </li>
+            <li>
+                <a href="/">E-Library</a>
+            </li>
+            <li>
+                <a href="/">Contact Us</a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Spa Factory Bali') }}</title>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <x-navbar />
+
+    <main>
+        {{ $slot }}
+    </main>
+</body>
+
+</html>
