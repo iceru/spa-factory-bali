bb0779d5db87311b422297129b2ed0e78d08888c
Muhamad Hafiz <m.hafiz1825@gmail.com>
2023-03-15 19:50:07 +0700

    about us animation

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,9 +1,9 @@
 <x-guest-layout>
-    <div class="about__header flex flex-col p-4 lg:px-0 lg:py-8 justify-end">
+    <div class="about__header flex flex-col p-4 lg:px-0 lg:py-8 justify-end" data-aos="fade-down">
         <div class="text-white container z-10 relative">
             <div class="lg:w-2/4">
-                <p class="lg:text-lg">Our Message is Simple:</p>
-                <div class="font-serif text-2xl lg:text-3xl">
+                <p class="lg:text-lg" data-aos="fade-right" data-aos-delay="400">Our Message is Simple:</p>
+                <div class="font-serif text-2xl lg:text-3xl" data-aos="fade-right" data-aos-delay="800">
                     We aim to be kind to the environment and respect all living beings – no testing on animals.
                 </div>
             </div>
@@ -12,13 +12,13 @@
     <div class="section">
         <div class="container grid lg:grid-cols-2 gap-4">
             <div class="p-0 lg:p-6 lg:pl-0">
-                <img src="/images/about/about-1.jpg" alt="About Spa Factory Bali" />
+                <img src="/images/about/about-1.jpg" alt="About Spa Factory Bali" data-aos="fade-right" />
             </div>
-            <div class="bg-diamond text-white flex flex-col justify-center p-8 ">
-                <div class="font-serif text-2xl mb-3">
+            <div class="bg-diamond text-white flex flex-col justify-center p-8 " data-aos="fade-left">
+                <div class="font-serif text-2xl mb-3" data-aos="fade-left" data-aos-delay="400">
                     About Spa Factory Bali
                 </div>
-                <p class="text-gray-300 text-justify">
+                <p class="text-gray-300 text-justify" data-aos="fade-left" data-aos-delay="800">
                     We started out as a cottage industry within a small house in 2002 targeted for tourist attraction.
                     Back then, we only made simple products like scrub powder, massage oil and bath salt. We expanded
                     into a more advanced and innovated cosmetic manufacturer as we relocated to our new factory to
@@ -32,10 +32,10 @@
     <div class="section">
         <div class="container grid lg:grid-cols-2 gap-4">
             <div class="flex flex-col justify-center p-0 lg:p-4 pl-0 order-2 lg:order-1">
-                <div class="font-serif text-2xl mb-3 text-primary ">
+                <div class="font-serif text-2xl mb-3 text-primary" data-aos="fade-right">
                     Empowering Women
                 </div>
-                <p class="text-justify text-body ">
+                <p class="text-justify text-body " data-aos="fade-right" data-aos-delay="400">
                     Spa Factory Bali is a company run by women who are determined to make a difference by empowering
                     women and supporting the local economy. Thus, we are employing many mothers with children who need
                     the income to support the family from the surrounding area. We support their determination to make a
@@ -43,20 +43,20 @@
                 </p>
             </div>
             <div class="p-0 lg:p-6 lg:pr-0 order-1 lg:order-2">
-                <img src="/images/about/about-2.png" alt="Empowering Women" />
+                <img src="/images/about/about-2.png" alt="Empowering Women" data-aos="fade-left" data-aos-delay="400" />
             </div>
         </div>
     </div>
     <div class="section">
         <div class="container grid lg:grid-cols-2 gap-4">
             <div class="p-0 lg:p-6 lg:pl-0">
-                <img src="/images/about/about-3.jpg" alt="About Spa Factory Bali" />
+                <img src="/images/about/about-3.jpg" alt="About Spa Factory Bali" data-aos="fade-right" />
             </div>
-            <div class="bg-diamond text-white flex flex-col justify-center p-8 ">
-                <div class="font-serif text-2xl mb-3">
+            <div class="bg-diamond text-white flex flex-col justify-center p-8 " data-aos="fade-left">
+                <div class="font-serif text-2xl mb-3" data-aos="fade-left" data-aos-delay="400">
                     Learning Organisation
                 </div>
-                <p class="text-gray-300 text-justify">
+                <p class="text-gray-300 text-justify" data-aos="fade-left" data-aos-delay="800">
                     Aiming to always proactively achieve incremental improvements to our everyday manufacturing process,
                     we proudly adapt Kaizen philosophy. We believe that it is important to create a culture of
                     continuous improvement where all employees are actively engaged in improving the company and stick
@@ -65,16 +65,16 @@
             </div>
         </div>
     </div>
-    <div class="section bg-linear">
+    <div class="section bg-linear" data-aos="fade-down">
         <div class="container">
-            <div class="font-serif text-white mb-4 text-2xl text-center">
+            <div class="font-serif text-white mb-4 text-2xl text-center" data-aos="fade-down" data-aos-delay="300">
                 Transparent and Traceable Production Process
             </div>
-            <div class="text-white text-center">
+            <div class="text-white text-center" data-aos="fade-down" data-aos-delay="600">
                 In line with the trend of ‘Farm to Beauty’, Spa Factory Bali Provides transparency – traceability of
                 production process and supply chain to ensure the purity and quality of active ingredients used.
             </div>
-            <div class="flex my-8 justify-between about__traceWrapper">
+            <div class="flex my-8 justify-between about__traceWrapper" data-aos="fade-down" data-aos-delay="900">
                 <div class="z-10 about__process active" id="process-1">
                     <div class="flex text-white mb-4 font-serif items-center justify-center">
                         <div class="mr-3">
@@ -112,15 +112,16 @@
                     </div>
                 </div>
             </div>
-            <div>
+            <div data-aos="fade-down">
                 <div class="process-1 about__process-large relative" style="height: 600px">
-                    <div class="absolute h-1/2 w-full bg-gradient-to-b from-black left-0 top-0">
+                    <div class="absolute h-1/2 w-full bg-gradient-to-b from-black left-0 top-0" data-aos="fade"
+                        data-aos-delay="200">
                     </div>
                     <div class="absolute top-8 left-8 z-10">
-                        <div class="font-serif text-2xl text-white">
+                        <div class="font-serif text-2xl text-white" data-aos="fade-right" data-aos-delay="400">
                             Process Name
                         </div>
-                        <div class="text-white">
+                        <div class="text-white" data-aos="fade-right" data-aos-delay="800">
                             Lorem ipsum dolor sit amet consectetur. Volutpat lectus semper praesent sit viverra
                             pellentesque augue maecenas blandit. At diam tellus ultricies dolor odio dictumst sem
                             cursus. Rutrum ac neque sit quisque vitae ac arcu. Tristique mattis in vitae etiam feugiat
@@ -137,17 +138,18 @@
     <div class="section">
         <div class="container grid lg:grid-cols-2 gap-4">
             <div class="flex flex-col justify-center p-0 lg:p-4 pl-0 order-2 lg:order-1">
-                <div class="font-serif text-2xl mb-3 text-primary ">
+                <div class="font-serif text-2xl mb-3 text-primary " data-aos="fade-right">
                     Following European Cosmetic Safety Guidance
                 </div>
-                <p class="text-justify text-body ">
+                <p class="text-justify text-body " data-aos-delay="400" data-aos="fade-right">
                     In-house R&D team with cosmetic competency and a formulator consultant in Netherland. We used only
                     natural ingredients and naturally derivative chemicals. Non-toxic, harmful chemicals and animal
                     derivative ingredients.
                 </p>
             </div>
             <div class="p-0 lg:p-6 lg:pr-0 order-1 lg:order-2">
-                <img src="/images/about/about-4.png" class="w-full" alt="Following European Cosmetic Safety Guidance" />
+                <img src="/images/about/about-4.png" class="w-full" alt="Following European Cosmetic Safety Guidance"
+                    data-aos="fade-left" data-aos-delay="400" />
             </div>
         </div>
     </div>
