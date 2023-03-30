9a16f28eb9a1744fe6bc3d5a2a1d80c1b06eaf02
Muhamad Hafiz <m.hafiz1825@gmail.com>
2023-03-30 16:01:39 +0700

    fix article link

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -8,11 +8,14 @@
             <div class="absolute bottom-0 w-full bg-gradient-to-t from-secondary h-1/2"></div>
 
             <div class="absolute bottom-8 text-white container left-1/2 transform -translate-x-1/2 px-4 lg:px-0">
-                <div class="font-bold text-4xl mb-4">
-                    {{ $item->title }}
-                </div>
+                <a href="{{ route('article.detail', $item->slug) }}">
+                    <div class="font-bold text-4xl mb-4">
+                        {{ $item->title }}
+                    </div>
+                </a>
+
                 <div class="mb-4">
-                    {!! $item->text !!}
+                    {!! $item->subtext !!}
                 </div>
                 <a href="{{ route('article.detail', $item->slug) }}">
                     <div class="flex items-center">
@@ -30,17 +33,31 @@
             <div class="grid lg:grid-cols-2">
                 @foreach ($featured->slice(0, 1) as $item)
                     <div class="text-white">
-                        <img src="{{ Storage::url($item->image) }}" class="mb-6" alt="{{ $item->title }}">
-                        <div class="font-bold text-2xl mb-2">
-                            {{ $item->title }}
-                        </div>
+                        <a href="{{ route('article.detail', $item->slug) }}">
+                            <img src="{{ Storage::url($item->image) }}" class="mb-6" alt="{{ $item->title }}">
+                        </a>
+
+                        <a href="{{ route('article.detail', $item->slug) }}">
+                            <div class="font-bold text-2xl mb-2">
+                                {{ $item->title }}
+                            </div>
+                        </a>
+
                         <div class="mb-2 text-sm">
                             by <span class="font-bold">{{ $item->author }}</span> |
                             {{ \Carbon\Carbon::create($item->created_at)->toFormattedDateString() }}
                         </div>
-                        <div>
-                            {!! $item->text !!}
+                        <div class="mb-2">
+                            {!! $item->subtext !!}
                         </div>
+                        <a href="{{ route('article.detail', $item->slug) }}">
+                            <div class="flex items-center">
+                                <div>
+                                    Read More
+                                </div>
+                                <img src="/images/read-more.png" alt="Arrow Right" class="ml-2">
+                            </div>
+                        </a>
                     </div>
                 @endforeach
             </div>
@@ -54,20 +71,26 @@
         @foreach ($latest as $item)
             <div class="flex items-center flex-wrap mb-8">
                 <div class="w-full lg:w-1/3 mb-6 lg:mb-0">
-                    <img src="{{ Storage::url($item->image) }}" alt="{{ $item->title }}">
+                    <a href="{{ route('article.detail', $item->slug) }}">
+                        <img src="{{ Storage::url($item->image) }}" alt="{{ $item->title }}">
+                    </a>
+
                 </div>
                 <div class="w-full lg:w-2/3 lg:pl-6">
-                    <div class="font-bold text-2xl mb-2">
-                        {{ $item->title }}
-                    </div>
+                    <a href="{{ route('article.detail', $item->slug) }}">
+                        <div class="font-bold text-2xl mb-2">
+                            {{ $item->title }}
+                        </div>
+                    </a>
+
                     <div class="text-primary mb-2">
                         by <span class="font-bold">{{ $item->author }}</span> |
                         {{ \Carbon\Carbon::create($item->created_at)->toFormattedDateString() }}
                     </div>
                     <div class="text-body mb-2">
-                        {!! $item->text !!}
+                        {!! $item->subtext !!}
                     </div>
-                    <a href="">
+                    <a href="{{ route('article.detail', $item->slug) }}">
                         <div class="flex items-center text-primary font-semibold">
                             <div>
                                 Read More
