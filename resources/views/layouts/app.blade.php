03dd491c0d89c8a975f4f18daff3ccbfcfcf6ede
Muhamad Hafiz <m.hafiz1825@gmail.com>
2023-05-13 23:01:22 +0700

    add fullscreen tinymce

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,13 +77,12 @@
 <script>
     tinymce.init({
         selector: 'textarea',
-        plugins: 'link image imagetools paste forecolor lists table',
+        plugins: 'link image imagetools paste lists table fullscreen',
         paste_as_text: true,
-        toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+        toolbar: 'undo redo | bold italic underline strikethrough | fullscreen | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl',
         a11y_advanced_options: true,
-        toolbar_mode: 'floating',
         tinycomments_mode: 'embedded',
-        tinycomments_author: 'UKMJAGOWAN.ID',
+        tinycomments_author: 'Spa Factory Bali',
         height: "350",
         color_cols: 5,
         color_map: [
